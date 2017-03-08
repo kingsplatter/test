571f39f71f5dd0dc3d9a8326f9eb7eb67e26344b
kingsplatter <andrew.strange@nwktc.edu>
2017-03-08 09:56:22 -0700

    Initial Commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by Andrew Strange on 3/8/17.
+//  Copyright © 2017 e. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
